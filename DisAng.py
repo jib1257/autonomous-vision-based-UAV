7a5c7ca45a64b2bd802f5e869e66371eaa079e9b
Bingchen Ji <jjjbbbccc1257@gmail.com>
2018-08-08 15:31:56 -0400

    Update DisAng.py

--- DisAng.py
+++ DisAng.py
@@ -1,10 +1,18 @@
 import math
-def pix2DisAng(camheight, camwidth, ptA, ptB, SIZE1m, pix_off_1m_arctanhalf):
-    mid = ((ptA[0]+ptB[0])/2,(ptA[1]+ptB[1])/2)
-    size = abs(ptB[0]-ptA[0])
-    dist = C*size
-    yaw = mid[1] - camwidth/2.0
-    disAng = [dist,yaw]
-    return disAng
 
-    
+#def measure1m():
+
+#Before using, measure the pixel width when object is at 1m, and measure the object width in meters
+
+def pix2DisAng(camwidth, ptA, ptB, LENGTH1m_pix, OBJECT_WIDTH_in_Meters):
+    mid = ((ptA[0]+ptB[0])/2,(ptA[1]+ptB[1])/2)
+    yawpix = mid[1] - camwidth/2.0
+
+    len_pix = abs(ptB[0]-ptA[0])
+    dist_in_m = LENGTH1m_pix/len_pix
+    len_per_pix = OBJECT_WIDTH_in_Meters/len_pix
+    offset_in_m = yawpix*len_per_pix
+    yawRad = math.atan2(offset_in_m,dist_in_m)
+    yawDeg = math.degrees(yawRad)
+
+    return [dist_in_m, yawDeg]
