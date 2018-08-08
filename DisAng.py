232ff1e03d9875c732499b9288ff15d5f1ac83c9
Bingchen Ji <jjjbbbccc1257@gmail.com>
2018-08-08 15:34:35 -0400

    Update DisAng.py

--- DisAng.py
+++ DisAng.py
@@ -4,6 +4,7 @@ import math
 
 #Before using, measure the pixel width when object is at 1m, and measure the object width in meters
 
+#Computes the distance and angle offset (in deg) using the camera inputs, object_length_at_1m_in_pixels and object_width_in_meters
 def pix2DisAng(camwidth, ptA, ptB, LENGTH1m_pix, OBJECT_WIDTH_in_Meters):
     mid = ((ptA[0]+ptB[0])/2,(ptA[1]+ptB[1])/2)
     yawpix = mid[1] - camwidth/2.0
